5840da90cfe9075d722c262e51d54c5cd98ece76
chelsysan <98607062+chelsysan@users.noreply.github.com>
2024-04-03 22:08:23 -0400

    updates (mouth/eye shuffler)

--- project-2/script.js
+++ project-2/script.js
@@ -43,6 +43,7 @@ let colorPicker = document.getElementById("color-picker");
 function bodyColorChange() {
     console.log(colorPicker.value);
     body.style.backgroundColor = colorPicker.value;
+    // document.body.style.backgroundColor = colorPicker.value;
 }
 
 //color picker for arms//
@@ -54,3 +55,72 @@ function armsColorChange() {
     arm1.style.backgroundColor = colorPickerArms.value;
     arm2.style.backgroundColor = colorPickerArms.value;
 }
+
+//color picker for legs//
+let leg1 = document.getElementById("leg1");
+let leg2 = document.getElementById("leg2");
+let colorPickerLegs = document.getElementById("color-picker-legs");
+
+let foot1 = document.getElementById("foot1");
+let foot2 = document.getElementById("foot2");
+
+function legsColorChange() {
+    leg1.style.backgroundColor = colorPickerLegs.value;
+    leg2.style.backgroundColor = colorPickerLegs.value;
+    foot1.style.backgroundColor = colorPickerLegs.value;
+    foot2.style.backgroundColor = colorPickerLegs.value;
+}
+
+//eye selector//
+let lefteye = document.getElementById("eye1");
+let righteye = document.getElementById("eye2");
+
+let eyebtnforward = document.getElementById("shuffle-eye-forward");
+let eyebtnbackwards = document.getElementById("shuffle-eye-backwards");
+let eyeArray = ["•", "^", "-", ">","<", "o", "T", "v", "=", ",", "~", "!", "*", "+", "x"]
+
+let index = 0
+
+eyebtnforward.onclick = function() {
+    lefteye.innerHTML = eyeArray[index];
+    righteye.innerHTML = eyeArray[index];
+    index = index + 1
+
+    if (index > 14) {
+        index = 0
+    }
+}
+
+/* eyebtnbackwards.onclick = function() {
+    lefteye.innerHTML = eyeArray[index];
+    righteye.innerHTML = eyeArray[index];
+    index = index - 1
+
+    if (index > 14) {
+        index = 0
+    }
+} */
+
+//mouth selector//
+let mouth = document.getElementById("mouth");
+let mouthShuffler = document.getElementById("mouth-shuffler");
+
+let mouthArray = [".", "w", "0", "u", "x"]
+
+
+mouthShuffler.onclick = function() {
+    mouth.innerHTML = mouthArray[index];
+    index = index + 1
+
+    if (index > 4) {
+        index = 0
+    }
+}
+
+
+
+
+
+
+
+
